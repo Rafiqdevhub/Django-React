cc71b24d1edbc9f008703e5976b413979763ea5c
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-02-07 14:43:48 +0500

    Enhance NotFound page with a user-friendly message and navigation link

--- forntend/src/pages/NotFound.jsx
+++ forntend/src/pages/NotFound.jsx
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>404</h1>
+      <p>Oops! Page not found.</p>
+      <Link to="/">Go Home</Link>
+    </div>
+  );
 };
 
 export default NotFound;
